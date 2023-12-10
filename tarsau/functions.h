338438f9dd779264c788eca620bdde361b9213b7
Halil İbrahim Üstün <hustun1@ford.com.tr>
2023-12-11 02:27:29 +0300

    main.c: - Refactored the command-line argument parsing to support unordered options. - Added checks to prevent multiple instances of '-b' and '-o' options. - Handled missing filenames after '-o' option. - Fixed a potential bug where the '.sau' extension could be appended multiple times to the output filename.
    functions.h:
    Refactored `createHeader` function:
       - Now uses `stat` to get file size and permissions directly, reducing file I/O operations.
       - Added error handling for `stat`.
       - Changed the function to return an integer for success or failure status.
    Modified `readAndWrite` function:
       Changed function signature to return an integer indicating success or failure.
    Included `endsWith` function:
       - Implemented to check if a string ends with a given suffix.
    
    - Added close statements in error cases of files not being correctly opened.

--- tarsau/functions.h
+++ tarsau/functions.h
@@ -2,13 +2,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 int isTextFile(const char *filename) {
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         perror("Error opening file");
         return -1;
     }
-    int isText = 1; 
+    int isText = 1;
     int ch;
     while ((ch = fgetc(file)) != EOF) {
         if (ch < 32 && ch != '\n' && ch != '\r' && ch != '\t') {
@@ -17,77 +18,92 @@ int isTextFile(const char *filename) {
         }
     }
     fclose(file);
-    return isText;  
+    return isText;
 }
 
-void createHeader(const char* filename,const char* saufile){
-	FILE *sectionFile = fopen(saufile,"a+");
-	FILE *readFile = fopen(filename,"r");
-	if (sectionFile == NULL) {
-        perror("Error opening file");
+int createHeader(const char *filename, const char *archive_file_name) {
+    struct stat fileStat;
+    if (stat(filename, &fileStat) != 0) {
+        perror("Error getting file stats");
         return -1;
     }
+    int perms = fileStat.st_mode & 0777;
+    long size = fileStat.st_size;
+
+    FILE *archive_file = fopen(archive_file_name, "a+");
+    if (archive_file == NULL) {
+        perror("Error opening archive file");
+        return -1;
+    }
+
+    fprintf(archive_file, "%s,%o,%ld|", filename, perms, size);
+    fclose(archive_file);
+
+    return 0;
+}
+
+
+long getSize(const char *filename) {
+    FILE *readFile = fopen(filename, "r");
     if (readFile == NULL) {
         perror("Error opening file");
         return -1;
     }
-	fseek(readFile,0,SEEK_END);
-	long size = ftell(readFile);
-	int perms;
-	struct stat fileStat;
-	if (stat(filename, &fileStat) == 0) {
-        perms = fileStat.st_mode & 0777;
-    }
-    fseek(readFile,0,SEEK_SET);
-	fprintf(sectionFile,"%s,%o,%ld|",filename,perms,size);
-	fclose(sectionFile);
-	fclose(readFile);
-}
-long getSize(const char* filename){
-	FILE *readFile = fopen(filename,"r");
-	if (readFile == NULL) {
-        perror("Error opening file");
-        return -1;
-    }
-	long size = fseek(readFile,0,SEEK_END);
-	fclose(readFile);
-	return size;
-}
-void readAndWrite(const char* filename, const char* saufile){
-	FILE *readFile = fopen(filename,"r");
-	FILE *sectionFile = fopen(saufile,"a+");
-	if (readFile == NULL) {
-        perror("Error opening file");
-        return -1;
-    }
-    if (sectionFile == NULL) {
-        perror("Error opening file");
-        return -1;
-    }
-	char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-	while ((read = getline(&line, &len, readFile)) != -1) {
-        fputs(line, sectionFile);
-    }
-    fclose(sectionFile);
-	fclose(readFile);
-	 if (line != NULL) {
-        free(line);
-    }
+    long size = fseek(readFile, 0, SEEK_END);
+    fclose(readFile);
+    return size;
 }
 
-char* getHeaderContent(const char* saufile){
-	FILE *sectionFile = fopen(saufile,"r");
-	if (sectionFile == NULL) {
-        perror("Error opening file");
+int readAndWrite(const char *file_to_be_archived_name, const char *archive_file_name) {
+    FILE *file_to_be_archived = fopen(file_to_be_archived_name, "r");
+    if (file_to_be_archived == NULL) {
+        perror("Error opening source file");
         return -1;
     }
+
+    FILE *archive_file = fopen(archive_file_name, "a+");
+    if (archive_file == NULL) {
+        perror("Error opening archive file");
+        fclose(file_to_be_archived);
+        return -1;
+    }
+
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
-	read = getline(&line, &len, readFile);
-	fclose(sectionFile);
-	return line;
+    while ((read = getline(&line, &len, file_to_be_archived)) != -1) {
+        fputs(line, archive_file);
+    }
+
+    free(line);
+    fclose(archive_file);
+    fclose(file_to_be_archived);
+
+    return 0;
 }
+
+int endsWith(const char *str, const char *suffix) {
+    if (!str || !suffix)
+        return 0;
+    size_t str_len = strlen(str);
+    size_t suffix_len = strlen(suffix);
+    if (suffix_len > str_len)
+        return 0;
+    return strncmp(str + str_len - suffix_len, suffix, suffix_len) == 0;
+}
+
+
+//char* getHeaderContent(const char* saufile){
+//	FILE *sectionFile = fopen(saufile,"r");
+//	if (sectionFile == NULL) {
+//        perror("Error opening file");
+//        return -1;
+//    }
+//    char *line = NULL;
+//    size_t len = 0;
+//    ssize_t read;
+//	read = getline(&line, &len, readFile);
+//	fclose(sectionFile);
+//	return line;
+//}
 
--- tarsau/main.c
+++ tarsau/main.c
@@ -5,52 +5,102 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include "functions.h"
-int main(int argc, char** argv){
-	if(argc<2){
-		printf("Error: improper command!\n");
-		return -1;
-	}
-	if (strcmp(argv[1], "-b") == 0) {
-		char* saufilename = "a.sau";
-		bool oopt = (strcmp(argv[argc-2],"-o") == 0);
-		int numfile = argc-2;
-		
-		if(oopt){
-			saufilename = argv[argc-1];
-			strcat(saufilename,".sau");
-			numfile -= 2;
-		} 
-		if(numfile > 32){
-			printf("Error: This archiver can only archive 32 files at a time. Please reduce the number of files.\n");
-			return -1;
-		}
-		for(int i = 2; i<numfile+2;i++){
-			if(isTextFile(argv[i]) == 0){
-				printf("Error: %s input file format is	incompatible! \n",argv[i]);
-				return -1;
-			}
-		}
-		int totalSize = 0;
-		for(int i = 2; i<numfile+2;i++){
-			totalSize += getSize(argv[i]);
-		}
-		if(totalSize > (200*1024*1024)){
-			printf("Error: The data that you want to archive is larger than 200MiB\n");
-			return -1;
-		}
-		for(int i = 2; i<numfile+2;i++){
-			createHeader(argv[i],saufilename); 
-		}
-		FILE *file = fopen(saufilename,"a+");fprintf(file,"\n");fclose(file); //spacing after header info
 
-		for(int i = 2; i<numfile+2; i++){
-			readAndWrite(argv[i],saufilename); //write data into .sau file
-		}
-    } else if (strcmp(argv[1], "-a") == 0) {
-        
-    } else {	
-        printf("Error: There is no such an option in tarsau command! --> -b for archiving -a for extracting\n");
-    	return -1;
+int main(int argc, char **argv) {
+    // current working directory for test purposes, clear later.
+//    char cwd[1024];
+//    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+//        printf("Current working dir: %s\n", cwd);
+//    } else {
+//        perror("getcwd() error");
+//    }
+
+    if (argc < 2) {
+        printf("Error: improper command!\n");
+        return -1;
     }
-	return 0;
+
+    char *archive_file_name = "a.sau";
+    bool is_archiving = false;
+    bool o_opt = false;
+    int num_text_files = 0;
+    int totalSize = 0;
+
+    for (int i = 1; i < argc; i++) {
+//        char *argument = argv[i];
+//        printf("%s\n", argument);
+        if (strcmp(argv[i], "-b") == 0) {
+            if (is_archiving) {
+                printf("Error: Multiple -b commands are not allowed.\n");
+                return -1;
+            }
+            is_archiving = true;
+            continue;
+        }
+        if (strcmp(argv[i], "-o") == 0) {
+            if (o_opt) {
+                printf("Error: Multiple -o commands are not allowed.\n");
+                return -1;
+            }
+            if (i + 1 < argc) {
+                // Check if the file already ends with '.sau' if so do not add another '.sau'.
+                if ((endsWith(archive_file_name, ".sau") != 0)) {
+
+                    archive_file_name = malloc(strlen(argv[i + 1]) +
+                                               5); // I'm allocating new memory for filename + ".sau" + the null terminator
+                    if (!archive_file_name) {
+                        perror("Memory allocation failed");
+                        return -1;
+                    }
+                    sprintf(archive_file_name, "%s.sau", argv[++i]);
+                }
+                o_opt = true;
+            } else {
+                printf("Error: Missing filename after -o option.\n");
+                return -1;
+            }
+        }
+    }
+
+    if (is_archiving) {
+        for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-b") != 0 && strcmp(argv[i], "-o") != 0 && strcmp(argv[i - 1], "-o") != 0) {
+                if (isTextFile(argv[i]) == 0) {
+                    printf("Error: %s input file format is incompatible! \n", argv[i]);
+                    return -1;
+                }
+                num_text_files++;
+                totalSize += getSize(argv[i]);
+            }
+        }
+        if (num_text_files > 32) {
+            printf("Error: This archiver can only archive 32 files at a time. Please reduce the number of files.\n");
+            return -1;
+        }
+        if (totalSize > (200 * 1024 * 1024)) {
+            printf("Error: The data that you want to archive is larger than 200MiB\n");
+            return -1;
+        }
+        for (int i = 2; i < argc; i++) {
+            if (strcmp(argv[i], "-b") != 0 && strcmp(argv[i], "-o") != 0 && strcmp(argv[i - 1], "-o") != 0) {
+                createHeader(argv[i], archive_file_name);
+            }
+        }
+
+        FILE *file = fopen(archive_file_name, "a+");
+        fprintf(file, "\n");
+        fclose(file); //spacing after header info
+
+        for (int i = 2; i < argc; i++) {
+            if (strcmp(argv[i], "-b") != 0 && strcmp(argv[i], "-o") != 0 && strcmp(argv[i - 1], "-o") != 0) {
+                readAndWrite(argv[i], archive_file_name); //write data into .sau file
+            }
+        }
+    } else {
+        printf("Error: There is no such an option in tarsau command! --> -b for archiving -a for extracting\n");
+        return -1;
+    }
+    free(archive_file_name);
+    printf("SUCCESS! WOOHOOO!\n");
+    return 0;
 }
