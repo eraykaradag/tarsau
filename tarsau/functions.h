3c7297a3e4014abe991d521a6be2a2d8692316a4
halil <hustun1@ford.com.tr>
2023-12-13 05:17:24 +0300

    - Added '-a' option for extraction. - Refactored getHeaderContent function to accurately get the header content. - Added getHeaderSize function. - Added createDirectoryStructure function. - Added extractFiles function.

--- tarsau/functions.h
+++ tarsau/functions.h
@@ -2,6 +2,24 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <libgen.h>
+
+enum Mode{
+    NONE,
+    ARCHIVING,
+    EXTRACTING
+};
+
+typedef struct {
+    char *file_name;
+    mode_t permissions;
+    long size;
+} FileHeader;
+
+
+int HEADER_SIZE = 10;
 
 int isTextFile(const char *filename) {
     FILE *file = fopen(filename, "rb");
@@ -114,17 +132,216 @@ void truncateFile(char *file_name){
 }
 
 
-//char* getHeaderContent(const char* saufile){
-//	FILE *sectionFile = fopen(saufile,"r");
-//	if (sectionFile == NULL) {
-//        perror("Error opening file");
-//        return -1;
-//    }
-//    char *line = NULL;
-//    size_t len = 0;
-//    ssize_t read;
-//	read = getline(&line, &len, readFile);
-//	fclose(sectionFile);
-//	return line;
-//}
+int getHeaderContent(FileHeader **headers, int *num_headers, const char *archive_file_name) {
+    FILE *archive_file = fopen(archive_file_name, "r");
+    if (archive_file == NULL) {
+        perror("Error opening archive file");
+        return -1;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    if (getline(&line, &len, archive_file) == -1) {
+        perror("Error reading header");
+        fclose(archive_file);
+        return -1;
+    }
+
+    if (line[strlen(line) - 1] == '\n') {
+        line[strlen(line) - 1] = '\0';
+    }
+
+    char *header_part = line + HEADER_SIZE + 1;
+    char *saveptr;
+    char *token = strtok_r(header_part, "|", &saveptr);
+    int idx = 0;
+
+    while (token) {
+        if (strlen(token) == 0) {
+            token = strtok_r(NULL, "|", &saveptr);
+            continue;
+        }
+
+        *headers = realloc(*headers, (idx + 1) * sizeof(FileHeader));
+        if (*headers == NULL) {
+            perror("Memory allocation failed for headers");
+            fclose(archive_file);
+            free(line);
+            return -1;
+        }
+
+        FileHeader *header = &((*headers)[idx]);
+        header->file_name = NULL;
+        sscanf(token, "%m[^,],%o,%ld", &header->file_name, &header->permissions, &header->size);
+        if (header->file_name == NULL) {
+            // Clean up
+            for (int i = 0; i < idx; i++) {
+                free((*headers)[i].file_name);
+            }
+            free(*headers);
+            fclose(archive_file);
+            free(line);
+            return -1;
+        }
+
+        idx++;
+        token = strtok_r(NULL, "|", &saveptr);
+    }
+
+    *num_headers = idx;
+    fclose(archive_file);
+    free(line);
+
+    return 0;
+}
+
+int getHeaderSize(const char* archive_file_name){
+    FILE *archive_file = fopen(archive_file_name,"r");
+    if (archive_file == NULL) {
+        perror("Error opening file");
+        fclose(archive_file);
+        return -1;
+    }
+    char header_size_str[11] = {0}; // first 10 byte + 1 for null terminator
+    size_t result = fread(header_size_str,1, HEADER_SIZE, archive_file);
+    fclose(archive_file);
+    if (result != 10){
+        perror("Error reading header size\n");
+        fclose(archive_file);
+        return -1;
+    }
+    char *endptr;
+    errno = 0;
+    long val = strtol(header_size_str, &endptr, 10);
+
+    if (errno != 0) {
+        perror("strtol");
+        return -1;
+    }
+    if (endptr == header_size_str) {
+        fprintf(stderr, "No digits were found\n");
+        return -1;
+    }
+
+    if (val < INT_MIN || val > INT_MAX) {
+        fprintf(stderr, "Header size out of int range\n");
+        return -1;
+    }
+    return (int)val;
+}
+
+int createDirectoryStructure(const char* path) {
+    char* temp_path = strdup(path);
+    if (temp_path == NULL) {
+        perror("Failed to duplicate path");
+        return -1;
+    }
+
+    // dirname can modify the input string, so we use a copy
+    char* dir_path = dirname(temp_path);
+
+    // Check if the directory already exists
+    struct stat st = {0};
+    if (stat(dir_path, &st) == -1) {
+        // Create the directory if it doesn't exist
+        if (mkdir(dir_path, 0777) != 0) {
+            perror("Failed to create directory");
+            free(temp_path);
+            return -1;
+        }
+    }
+
+    free(temp_path);
+    return 0;
+}
+
+int extractFiles(const char* archive_file_name, const char* extract_directory) {
+    FileHeader *headers = NULL;
+    int num_headers = 0;
+
+    // Get header size from archive file
+    int header_size = getHeaderSize(archive_file_name);
+    if (header_size < 0) {
+        return -1; // Error message already printed by getHeaderSize
+    }
+
+    // Get header content from archive file
+    if (getHeaderContent(&headers, &num_headers, archive_file_name) != 0) {
+        return -1; // Error message already printed by getHeaderContent
+    }
+
+    for (int i = 0; i < num_headers; ++i) {
+        char* f = headers[i].file_name;
+        mode_t p = headers[i].permissions;
+        long s = headers[i].size;
+    }
+
+    // Open the archive file for reading file contents
+    FILE *archive_file = fopen(archive_file_name, "r");
+    if (archive_file == NULL) {
+        perror("Error opening archive file");
+        free(headers);
+        return -1;
+    }
+
+    // Skip the header section in the archive file
+    fseek(archive_file, header_size + 1, SEEK_SET);
+
+    for (int i = 0; i < num_headers; i++) {
+        char full_path[PATH_MAX];
+        snprintf(full_path, sizeof(full_path), "%s/%s", extract_directory, headers[i].file_name);
+
+        // Create directory structure for the file
+        if (createDirectoryStructure(full_path) != 0) {
+            fclose(archive_file);
+            free(headers);
+            return -1;
+        }
+
+        // Open file for writing in text mode
+        char* path = full_path;
+        FILE *out_file = fopen(full_path, "w");
+        if (out_file == NULL) {
+            perror("Error opening output file");
+            fclose(archive_file);
+            free(headers);
+            return -1;
+        }
+
+        // Read and write the file content
+        char buffer[1024];
+        long bytes_to_read = headers[i].size;
+        while (bytes_to_read > 0) {
+            size_t bytes_to_read_now = (bytes_to_read < sizeof(buffer)) ? bytes_to_read : sizeof(buffer);
+            size_t bytes_read = fread(buffer, 1, bytes_to_read_now, archive_file);
+
+            if (bytes_read == 0) {
+                break;
+            }
+
+            fwrite(buffer, 1, bytes_read, out_file);
+            bytes_to_read -= bytes_read;
+        }
+
+        fclose(out_file);
+
+        // Set the file permissions to match the header
+        if (chmod(full_path, headers[i].permissions) == -1) {
+            perror("Error setting file permissions");
+            fclose(archive_file);
+            free(headers);
+            return -1;
+        }
+    }
+
+    fclose(archive_file);
+
+    // Clean up allocated headers
+    for (int i = 0; i < num_headers; i++) {
+        free(headers[i].file_name);
+    }
+    free(headers);
+
+    return 0; // Success
+}
 
